4f3ef0b46b5e0cf764406d84cdb0639243b1b87f
Daniel A Kaiser <dank2878@gmail.com>
2011-04-14 15:15:50 -0400

    commit source

---
+++ CS372-Assn4-Prog.cpp
@@ -0,0 +1,81 @@
+// ----------------------------------------------------------------------------
+// CODE FILENAME: 	CS372-Assn4-Prog.cpp
+//
+// DESCRIPTION:
+//		Program to test multiple data sorting methods
+//
+// DATE: 			May 1, 2011
+// DESIGNER: 		Daniel Kaiser & David Vasiliu
+// FUNCTIONS:
+//		[fuction name - description]
+// ----------------------------------------------------------------------------
+
+// Preprocessor directives-----------------------------------------------------
+#include <iostream>         // for standard I/O
+#include <fstream>			// for file I/O
+#include <iomanip>			// for formatting output
+#include <string>			// for sting use
+#include <cmath>			// for mathematical functions
+#include <cctype>           // for character comparisons
+#include <cstring>          // for string comparisons
+#include <ctime>			// for system clock count
+
+using namespace std;
+// User defined function prototypes--------------------------------------------
+
+// Named constants-------------------------------------------------------------
+
+// ----------------------------------------------------------------------------
+// FUNCTION:	main
+//
+// DESCRIPTION:	[explain what fucntion does and how it works]
+//
+// INPUT:
+//		Parameters: [name - description]
+//		File:		[name - description]
+//
+// OUTPUT:
+//		Return Value:	[name - description]
+//									   
+// CALLS TO:
+//		[function name(s)]
+// ----------------------------------------------------------------------------
+
+int main() 
+{
+     // local variables---------------------------------------------------------
+    char ch;		 		// Variable for system pause on Linux/OSX
+	
+    // ------------------------------------------------------------------------
+	// System pause for Linux/OS X systems - uncomment following
+	// three lines for use    
+    cout << endl;
+    cout << "Press [ENTER] to exit" << endl;
+    cin.ignore(200, '\n');     					// Clean out the input stream
+    cin.get(ch);
+    // ------------------------------------------------------------------------
+    
+    return 0;
+}
+
+// ----------------------------------------------------------------------------
+// FUNCTION:	[name]
+//
+// DESCRIPTION:	[explain what fucntion does and how it works]
+//
+// INPUT:
+//		Parameters: [name - description]
+//		File:		[name - description]
+//
+// OUTPUT:
+//		Parameters:		[name - description]
+//		File:			[name - description]
+//		Return Value:	[name - description]
+//									   
+// CALLS TO:
+//		[function name(s)]
+// ----------------------------------------------------------------------------
+
+//[function type] FunctionName (paramerters)
+
+	// local variables---------------------------------------------------------
